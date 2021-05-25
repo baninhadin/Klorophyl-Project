b9e6b60bb59f1fc1d6a1805ed3233f7e62fbf416
Banin Hadin <baninhadin@gmail.com>
2021-05-25 21:26:25 +0700

    Masih belum bisa fetch data hourly seharusnya bisa save data hourly

--- routes/api/data.js
+++ routes/api/data.js
@@ -5,12 +5,6 @@ const router = express.Router()
 const Data = require('../../models/Data')
 
 router.get('/', async (req, res) => {
-  let token = config.get('WAQI_KEY')
-  let link = 'https://api.waqi.info'
-  let endpoints = 'search'
-  let params = `keyword=Jakarta&token=${token}`
-  let realData
-
   try {
     let data = await Data.exists()
 
@@ -41,8 +35,18 @@ router.get('/', async (req, res) => {
   }
 
   try {
+    let token = config.get('WAQI_KEY')
+    let link = 'https://api.waqi.info'
+    let endpoints = 'search'
+    let params = `keyword=Jakarta&token=${token}`
+    let realData
     let rawData = await axios.get(`${link}/${endpoints}/?${params}`)
+    //     let endDate = new Date()
+    //     let startDate = new Date(endDate.getTime() - 1 * 24 * 60 * 60 * 1000)
+    let startDate = '2021-05-23'
+    let endDate = '2021-05-24'
     realData = rawData.data.data
+
     const promises = realData.map(async (datas) => {
       // Create new data
       let data = new Data({
@@ -54,52 +58,51 @@ router.get('/', async (req, res) => {
         date: datas.time.stime,
       })
 
-      // Fetch weather data from OPENWEATHER API
-      token = config.get('OPENWEATHER_KEY')
-      link = 'http://api.openweathermap.org/data/2.5'
-      endpoints = 'weather'
-      params = `lat=${data.geo.latitude}&lon=${data.geo.longitude}&appid=${token}`
-
-      let weatherData = await axios.get(`${link}/${endpoints}?${params}`)
-      data.weather.humidity = weatherData.data.main.humidity
-
       // Fetch weather data from WEATHERBIT API
       token = config.get('WEATHERBIT_KEY')
       link = 'http://api.weatherbit.io/v2.0'
-      endpoints = 'current'
-      params = `lat=${data.geo.latitude}&lon=${data.geo.longitude}&key=${token}`
+      endpoints = 'history/hourly'
+      params = `lat=${data.geo.latitude}&lon=${data.geo.longitude}&key=${token}&start_date=${startDate}&end_date=${endDate}`
 
       weatherData = await axios.get(`${link}/${endpoints}?${params}`)
 
-      data.weather.windDir = weatherData.data.data[0].wind_cdir_full
-      data.weather.windSpeed = weatherData.data.data[0].wind_spd
-      data.weather.precip = weatherData.data.data[0].precip
-      data.weather.temp = weatherData.data.data[0].temp
-      data.weather.desc = weatherData.data.data[0].weather.description
+      console.log('---------------------------------------------------------')
+      const weatherPromises = weatherData.data.data.map(async (item) => {
+        //    console.log(item.timestamp_utc)
+        let adaData = await Data.findOne({ date: item.timestamp_utc })
+        console.log(adaData)
+        if (!adaData) {
+          console.log('Data baru')
+          data.weather.windDir = item.wind_cdir_full
+          data.weather.windSpeed = item.wind_spd
+          data.weather.precip = item.precip
+          data.weather.temp = item.temp
+          data.weather.humidity = item.rh
+          data.weather.desc = item.weather.description
+          return data.save()
+        }
+      })
+
+      await Promise.all(weatherPromises)
 
       // Fetch air quality data from WEATHERBIT API
-      endpoints = 'current/airquality'
-      params = `lat=${data.geo.latitude}&lon=${data.geo.longitude}&key=${token}`
+      //  endpoints = 'history/airquality'
+      //  params = `lat=${data.geo.latitude}&lon=${data.geo.longitude}&key=${token}&start_date=${startDate}&end_date=${endDate}`
 
-      weatherData = await axios.get(`${link}/${endpoints}?${params}`)
+      //  weatherData = await axios.get(`${link}/${endpoints}?${params}`)
 
-      data.airQuality.aqi = weatherData.data.data[0].aqi
-      data.airQuality.no = weatherData.data.data[0].no
-      data.airQuality.o3 = weatherData.data.data[0].o3
-      data.airQuality.so2 = weatherData.data.data[0].no2
-      data.airQuality.no2 = weatherData.data.data[0].no2
-      data.airQuality.pm10 = weatherData.data.data[0].pm10
-      data.airQuality.pm25 = weatherData.data.data[0].pm25
+      //  data.airQuality.aqi = weatherData.data.data[0].aqi
+      //  data.airQuality.no = weatherData.data.data[0].no
+      //  data.airQuality.o3 = weatherData.data.data[0].o3
+      //  data.airQuality.so2 = weatherData.data.data[0].no2
+      //  data.airQuality.no2 = weatherData.data.data[0].no2
+      //  data.airQuality.pm10 = weatherData.data.data[0].pm10
+      //  data.airQuality.pm25 = weatherData.data.data[0].pm25
 
-      return data.save()
+      //  return data.save()
     })
 
-    await Promise.all(promises)
-
     let cobaData = await Data.find().select({ _id: 0, __v: 0 })
-    //     let cobaData2 = await Data.find()
-    console.log(cobaData)
-    //     console.log(cobaData2)
     res.json(cobaData)
   } catch (err) {
     console.error(err.message)
