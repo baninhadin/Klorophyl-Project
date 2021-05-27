6c7c8d6affd7563449b7cadf98608cfe3275fb59
Banin Hadin <baninhadin@gmail.com>
2021-05-28 00:08:48 +0700

    removing some code

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const config = require('config');
-const { check, validationResult } = require('express-validator');
-const normalize = require('normalize-url');
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const { check, validationResult } = require('express-validator')
+// const normalize = require('normalize-url');
 
-const User = require('../../models/User');
+const User = require('../../models/User')
 
 // Getting all
 router.get('/', async (req, res) => {
   try {
-    const users = await User.find();
-    res.json(users);
+    const users = await User.find()
+    res.json(users)
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ message: err.message })
   }
-});
+})
 
 // Getting One
 router.get('/:userName', getUser, (req, res) => {
-  res.json(res.user);
-});
+  res.json(res.user)
+})
 
 // Creating one
 router.post(
@@ -34,98 +34,98 @@ router.post(
   ).isLength({ min: 6 }),
   async (req, res) => {
     // If one of the validation returns an error
-    const errors = validationResult(req);
+    const errors = validationResult(req)
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() })
     }
 
-    const { userName, email, password } = req.body;
+    const { userName, email, password } = req.body
     try {
-      let user = await User.findOne({ userName });
+      let user = await User.findOne({ userName })
 
       if (user) {
         return res
           .status(400)
-          .json({ errors: [{ msg: 'User already exists' }] });
+          .json({ errors: [{ msg: 'User already exists' }] })
       }
 
       user = new User({
         userName,
         email,
         password,
-      });
+      })
 
-      const salt = await bcrypt.genSalt(10);
+      const salt = await bcrypt.genSalt(10)
 
-      user.password = await bcrypt.hash(password, salt);
+      user.password = await bcrypt.hash(password, salt)
 
-      await user.save();
+      await user.save()
 
       const payload = {
         user: {
           id: user.id,
         },
-      };
+      }
 
       jwt.sign(
         payload,
         config.get('jwtSecret'),
         { expiresIn: '5 days' },
         (err, token) => {
-          if (err) throw err;
-          res.json({ token });
+          if (err) throw err
+          res.json({ token })
         }
-      );
+      )
     } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server error');
+      console.error(err.message)
+      res.status(500).send('Server error')
     }
   }
-);
+)
 
 // Updating One
 router.patch('/:userName', getUser, async (req, res) => {
   if (req.body.userName != null) {
-    res.user.userName = req.body.userName;
+    res.user.userName = req.body.userName
   }
   if (req.body.email != null) {
-    res.user.email = req.body.email;
+    res.user.email = req.body.email
   }
   // if (req.body.fullName != null) {
   // res.user.fullName = req.body.fullName;
   // }
   try {
-    const updatedUser = await res.user.save();
-    res.json(updatedUser);
+    const updatedUser = await res.user.save()
+    res.json(updatedUser)
   } catch (err) {
-    res.status(400).json({ message: err.message });
+    res.status(400).json({ message: err.message })
   }
-});
+})
 
 // Deleting One
 router.delete('/:userName', getUser, async (req, res) => {
   try {
-    await res.user.remove();
-    res.json({ message: 'Deleted User' });
+    await res.user.remove()
+    res.json({ message: 'Deleted User' })
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ message: err.message })
   }
-});
+})
 
 // Middleware to get user
 async function getUser(req, res, next) {
-  let user;
+  let user
   try {
-    user = await User.findOne({ userName: req.params.userName });
+    user = await User.findOne({ userName: req.params.userName })
     if (user == null) {
-      return res.status(404).json({ message: 'Cannot find user' });
+      return res.status(404).json({ message: 'Cannot find user' })
     }
   } catch (err) {
-    return res.status(500).json({ message: err.message });
+    return res.status(500).json({ message: err.message })
   }
 
-  res.user = user;
-  next();
+  res.user = user
+  next()
 }
 
-module.exports = router;
+module.exports = router
